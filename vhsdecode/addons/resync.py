25086fcec160fc9e9dc1893c0d3d816813fea894
Sebastian Wilwerth <sebastian.wilwerth@gmail.com>
2021-03-26 19:37:54 -0300

    DC restore improved metrics
    Signed-off-by: Sebastian Wilwerth <sebastian.wilwerth@gmail.com>

--- vhsdecode/addons/resync.py
+++ vhsdecode/addons/resync.py
@@ -33,12 +33,21 @@ class DCrestore:
         self.vsyncFilter = FiltersClass(iir_vsync[0], iir_vsync[1], self.samp_rate)
         #filter_plot(iir_vsync[0], iir_vsync[1], self.samp_rate, type='lowpass', title='Vsync')
 
-    def get_rawlinelocs(self, data, rawdata):
+        self.eq_pulselen = round(t_to_samples(self.samp_rate, 1 / (self.SysParams["eqPulseUS"] * 1e-6)))
+        self.linelen = round(t_to_samples(self.samp_rate, self.fh))
+        self.last_lineoffset = 0
+
+    def gen_hsyncmask(self):
+        eq_pulse = np.zeros(self.eq_pulselen)
+        line_pulse = np.ones(self.linelen - self.eq_pulselen)
+        return np.append(eq_pulse, line_pulse)
+
+    def get_rawlinelocs(self, data, int_window=10):
         hsync = self.hsyncFilter.filtfilt(data)
         level = np.ones(len(data)) * self.scale(-self.SysParams["vsync_ire"] / 3)
         cross = hsync - level
         where = zero_cross_det(cross)
-        if len(where) > 8:
+        if len(where) > 20:
             where_diff = np.diff(where)
             end_points = argrelextrema(where_diff, np.greater)[0]
             start_points = argrelextrema(where_diff, np.less)[0]
@@ -52,34 +61,41 @@ class DCrestore:
                     end_points[len(end_points) - 1] = len(where) - 1
 
             gated_hsync = list()
-            sync_mins = list()
+            sync_min_holds = list()
+            sync_starts = list()
             for i, start in enumerate(start_points):
                 try:
                     s_p = min(start_points[i], end_points[i])
                     e_p = max(start_points[i], end_points[i])
                     hsync_area = data[where[s_p]:where[e_p]]
                     if len(hsync_area) > 0:
+
                         gated_hsync.append(hsync_area)
                         #plot_scope(hsync_area)
-                        hmin = min(hsync_area)
-                        sync_mins.append(hmin)
-                        print('Hslice min:', hmin)
+                        hmin_id = int(np.where(hsync_area == min(hsync_area))[0])
+                        hmin_id = max(hmin_id, int_window)
+                        hmin_id = min(hmin_id, len(hsync_area) - int_window - 1)
+                        sync_starts.append(where[s_p] + hmin_id)
+                        hmin = np.mean(hsync_area[hmin_id-int_window:hmin_id+int_window])
+                        sync_min_holds.append(hmin)
+                        print('Hslice min, id', hmin, where[s_p] + hmin_id)
 
                 except IndexError as e:
                     print(e)
                     print(len(where), start_points, end_points)
                     exit(0)
 
+            plot_scope(sync_min_holds)
+            print(len(sync_min_holds), sync_starts, np.diff(sync_starts))
             #plot_scope(result)
             #for id, pos in enumerate(where):
             #dualplot_scope(level[self.ignoresamples:], hsync[self.ignoresamples:])
             return None
         else:
-            eq_pulselen = round(t_to_samples(self.samp_rate, 1 / (self.SysParams["eqPulseUS"] * 1e-6)))
-
+            #plot_scope(self.gen_hsyncmask())
             print('cannot clamp signal')
             return None
 
     def work(self, data):
         deemph = self.deemphFilter.filtfilt(data)
-        locs = self.get_rawlinelocs(deemph, data)
+        locs = self.get_rawlinelocs(deemph)
