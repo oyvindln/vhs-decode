Refactored the moving average filter for re-utilisation on other code points. Added it to the AGC measuring values to filter out noise. It seems to fix/workaround AGC issues on the samples I have.
Signed-off-by: VideoMem <sebastian.wilwerth@gmail.com>

vhsdecode/addons/resync.py
@@ -5,44 +5,32 @@ import lddecode.utils as lddu
 import itertools
 from lddecode.utils import inrange
 import lddecode.core as ldd
-import json
 import math
 import hashlib
+
 
 # stores the last valid blacklevel, synclevel and vsynclocs state
 # preliminary solution to fix spurious decoding halts (numpy error case)
 class FieldState:
     def __init__(self):
-        self.blanklevels = np.array([])
-        self.synclevels = np.array([])
+        self.blanklevels = utils.StackableMA()
+        self.synclevels = utils.StackableMA()
         self.locs = None
-        self.field_average = 30
-        self.min_watermark = 3
 
     def setSyncLevel(self, level):
-        self.synclevels = np.append(self.synclevels, level)
+        self.synclevels.push(level)
 
     def setLevels(self, blank, sync):
-        self.blanklevels = np.append(self.blanklevels, blank)
+        self.blanklevels.push(blank)
         self.setSyncLevel(sync)
 
     def getSyncLevel(self):
-        if np.size(self.synclevels) > 0:
-            synclevel, self.synclevels = utils.moving_average(
-                self.synclevels, window=self.field_average
-            )
-            return synclevel
-        else:
-            return None
+        return self.synclevels.pull()
 
     def getLevels(self):
-        if np.size(self.blanklevels) > 0:
-            blacklevel, self.blanklevels = utils.moving_average(
-                self.blanklevels, window=self.field_average
-            )
-            return blacklevel, self.getSyncLevel()
-        else:
-            return None, None
+        blevels = self.blanklevels.pull()
+        if blevels is not None:
+            return blevels, self.getSyncLevel()
 
     def setLocs(self, locs):
         self.locs = locs
@@ -52,8 +40,8 @@ class FieldState:
 
     def hasLevels(self):
         return (
-            np.size(self.blanklevels) > self.min_watermark
-            and np.size(self.synclevels) > self.min_watermark
+            self.blanklevels.has_values()
+            and self.synclevels.has_values()
         )
 
 

vhsdecode/addons/vsyncserration.py
@@ -5,7 +5,7 @@ from vhsdecode.utils import (
     plot_scope,
     dualplot_scope,
     zero_cross_det,
-    moving_average,
+    StackableMA,
 )
 import numpy as np
 from scipy.signal import argrelextrema
@@ -56,27 +56,23 @@ class VsyncSerration:
         self.vsynclen = round(t_to_samples(self.samp_rate, self.fv))
         self.linelen = round(t_to_samples(self.samp_rate, self.fh))
         self.pid = getpid()
-        self.levels = list(), list()  # sync, blanking
-        self.level_average = 30
+        self.levels = StackableMA(min_watermark=2), StackableMA(min_watermark=2)  # sync, blanking
         self.sync_level_bias = np.array([])
         self.fieldcount = 0
-        self.min_watermark = 2
 
     def get_levels(self):
-        sync, sync_list = moving_average(self.levels[0], window=self.level_average)
-        blank, blank_list = moving_average(self.levels[1], window=self.level_average)
-        self.levels = sync_list, blank_list
+        sync, blank = self.levels[0].pull(), self.levels[1].pull()
         return sync, blank
 
     def has_levels(self):
         return (
-            len(self.levels[0]) > self.min_watermark
-            and len(self.levels[1]) > self.min_watermark
+            self.levels[0].has_values()
+            and self.levels[1].has_values()
         )
 
     def push_levels(self, levels):
         for ix, level in enumerate(levels):
-            self.levels[ix].append(level)
+            self.levels[ix].push(level)
 
     def mutemask(self, raw_locs, blocklen, pulselen):
         mask = np.zeros(blocklen)
@@ -258,7 +254,7 @@ class VsyncSerration:
             ldd.logger.debug(
                 "VBI serration levels %d - Sync tip: %.02f kHz, Blanking (ire0): %.02f kHz"
                 % (
-                    len(self.levels[0]),
+                    self.levels[0].size(),
                     self.get_levels()[0] / 1e3,
                     self.get_levels()[1] / 1e3,
                 )