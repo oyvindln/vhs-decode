9acfdd9cb0576fdcd05233db8ba8c5284f5d3f77
Zcooger <42012614+Zcooger@users.noreply.github.com>
2025-03-20 16:55:12 +0100

    Update mainwindow.h

--- tools/ld-analyse/mainwindow.h
+++ tools/ld-analyse/mainwindow.h
@@ -33,6 +33,7 @@
 #include <QMessageBox>
 #include <QLabel>
 #include <QMouseEvent>
+#include <QClipboard>
 
 #include "oscilloscopedialog.h"
 #include "vectorscopedialog.h"
@@ -75,6 +76,7 @@ private slots:
     void on_actionVisible_Dropout_analysis_triggered();
     void on_actionSNR_analysis_triggered();
     void on_actionWhite_SNR_analysis_triggered();
+    void on_actionCopy_frame_to_Clipboard_triggered();
     void on_actionSave_frame_as_PNG_triggered();
     void on_actionZoom_In_triggered();
     void on_actionZoom_Out_triggered();
