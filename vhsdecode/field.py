1820ca754e5d229103d29d1529cfdff6bc42235f
Ethan Halsall <ethanhalsall11@augustana.edu>
2026-07-23 01:13:21 -0500

    model transient response with attack and release

--- vhsdecode/field.py
+++ vhsdecode/field.py
@@ -11,7 +11,7 @@ import vhsdecode.sync as sync
 import vhsdecode.formats as formats
 from vhsdecode.doc import detect_dropouts_rf
 from vhsdecode.chroma import decode_chroma, decode_chroma_phase_rotation
-from vhsdecode.addons.ringing_cancellation import apply_tbc_ringing_correction, plot_tbc_correction_debug
+from vhsdecode.addons.ringing_cancellation import apply_tbc_vhs_deemphasis_correction
 
 from vhsdecode.debug_plot import plot_data_and_pulses
 
@@ -1142,39 +1142,26 @@ class FieldShared:
     def downscale(self, final=False, *args, **kwargs):
         dsout, dsaudio, dsefm = super(FieldShared, self).downscale(final=False, *args, **kwargs)
 
-        front_porch_len = 15
+        front_porch_len = 10
         sync_len = 67
-        back_porch_len = 67
+        back_porch_len = 30
         gain_scale = 1
         target_transition = 3.3
 
-        #plot_tbc_correction_debug(
-        #    dsout,
-        #    self.lineoffset,
-        #    self.linecount + self.lineoffset,
-        #    self.outlinelen,
-        #    self.blanking_level,
-        #    self.sync_tip_level,
-        #    front_porch_len=front_porch_len,
-        #    sync_len=sync_len,
-        #    back_porch_len=back_porch_len,
-        #    gain_scale=gain_scale,
-        #    target_transition=target_transition,
-        #)
-#
-        #dsout = apply_tbc_ringing_correction(
-        #    dsout,
-        #    self.lineoffset,
-        #    self.linecount + self.lineoffset,
-        #    self.outlinelen,
-        #    self.blanking_level,
-        #    self.sync_tip_level,
-        #    front_porch_len=front_porch_len,
-        #    sync_len=sync_len,
-        #    back_porch_len=back_porch_len,
-        #    gain_scale=gain_scale,
-        #    target_transition=target_transition,
-        #)
+        dsout = apply_tbc_vhs_deemphasis_correction(
+            dsout,
+            self.lineoffset,
+            self.linecount + self.lineoffset,
+            self.outlinelen,
+            self.blanking_level,
+            self.sync_tip_level,
+            front_porch_len=front_porch_len,
+            sync_len=sync_len,
+            back_porch_len=back_porch_len,
+            gain_scale=gain_scale,
+            target_transition=target_transition,
+            debug=True
+        )
 
         # hpf = utils.filter_simple(dsout, self.rf.Filters["NLHighPass"])
         # dsout = ynr(dsout, hpf, self.outlinelen)
